b9c654e710e852299c3823b67fc56c5ec6b12c4b
trungtan <trungtan14@gmail.com>
2024-09-11 14:14:02 +0700

    fixbug

--- routes/auth.js
+++ routes/auth.js
@@ -72,7 +72,7 @@ router.post('/login', async (req, res) => {
 
     res.status(200).json({
       user: {
-        id: user._id,
+        _id: user._id,
         name: user.name,
         email: user.email,
       },
