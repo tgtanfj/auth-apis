15a331fef64647457491e073d1565f1a72a6e3ba
trungtan <trungtan14@gmail.com>
2024-09-11 10:58:19 +0700

    add route

--- routes/user.js
+++ routes/user.js
@@ -12,4 +12,42 @@ router.get('/get-users', authenticateToken, async (req, res) => {
   }
 });
 
+router.get('/get-user-by-id/:id', authenticateToken, async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const user = await User.findById(id);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+
+router.put('/update-user-by-id/:id', authenticateToken, async (req, res) => {
+  try {
+    const { name, email, phone, address, age, job } = req.body;
+    const { id } = req.params;
+
+    const updatedUser = await User.findByIdAndUpdate(
+      id,
+      { name, email, phone, address, age, job },
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 module.exports = router;
