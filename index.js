56b211e9d735790b30b760e98bd5309d6ace255b
trungtan <trungtan14@gmail.com>
2024-09-11 10:39:10 +0700

    fix port

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(cors(corsOptions));
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
