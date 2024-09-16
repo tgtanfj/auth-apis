98136ba20dd1f90a18151d2b541422df130548f5
trungtan <trungtan14@gmail.com>
2024-09-16 13:47:43 +0700

    add pagination api

--- routes/user.js
+++ routes/user.js
@@ -3,14 +3,41 @@ const router = express.Router();
 const User = require('../models/User');
 const authenticateToken = require('../middleware/authenticateToken'); 
 
+// router.get('/get-users', authenticateToken, async (req, res) => {
+//   try {
+//     const users = await User.find({});
+//     res.status(200).json(users);
+//   } catch (err) {
+//     res.status(500).json({ message: err.message });
+//   }
+// });
+
 router.get('/get-users', authenticateToken, async (req, res) => {
   try {
-    const users = await User.find({});
-    res.status(200).json(users);
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+
+    const skip = (page - 1) * limit;
+
+    const users = await User.find({})
+      .skip(skip)
+      .limit(limit);
+
+    const totalUsers = await User.countDocuments();
+
+    const totalPages = Math.ceil(totalUsers / limit);
+
+    res.status(200).json({
+      users,
+      totalUsers,
+      totalPages,
+      currentPage: page,
+    });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 });
+
 
 router.get('/get-user-by-id/:id', authenticateToken, async (req, res) => {
   try {
